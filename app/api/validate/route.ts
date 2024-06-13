b5f623c0ca004385859a71482312007734a5e5d4
 <96326525+mutasim77@users.noreply.github.com>
2024-06-13 11:04:06 +0500

    🦄 feat: add geminiAI and create function for generating content

--- app/api/validate/route.ts
+++ app/api/validate/route.ts
@@ -1,10 +1,19 @@
-import convertJsonSchemaToZod from "@/app/lib/schemaUtils";
-import { generateContentText } from "@/app/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { z } from "zod";
+import {
+    convertJsonSchemaToZod,
+    RetryablePromise,
+    generateContentText,
+    PROMPT,
+    WELCOME_MESSAGE,
+} from "@/app/lib";
 
 export function GET() {
-    return new Response("Hello Bro!");
+    return new Response(WELCOME_MESSAGE, {
+        headers: { 'Content-Type': 'text/plain' },
+        status: 200
+    });
 }
 
 export async function POST(req: NextRequest) {
@@ -20,5 +29,35 @@ export async function POST(req: NextRequest) {
     const dynamicSchema = convertJsonSchemaToZod(format);
     const content = generateContentText({ data, format });
 
-    return NextResponse.json({ message: "Working Fine" }, { status: 200 })
+    /**
+     * This function uses the RetryablePromise utility to attempt the content generation 
+     * process up to three times in case of failures. It interacts with the geminiAI 
+     * library to generate content based on a given prompt and schema, then parses 
+     * the generated content using a dynamic schema.
+     */
+    const generateAndParseContent = await RetryablePromise.retry<string>(3, async (resolve, reject) => {
+        try {
+            const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+            const model = genAI.getGenerativeModel({
+                model: "gemini-1.5-flash",
+                systemInstruction: PROMPT,
+            });
+
+            //~~ Generate content using the generative model
+            const generationResult = await model.generateContent(content);
+            const generationResponse = generationResult.response;
+
+            //~~ Extract the generated text from the response
+            const generatedText = generationResponse.text();
+
+            //~~ Parse the generated text using the dynamic schema
+            const parsedResult = dynamicSchema.parse(JSON.parse(generatedText || ""));
+
+            return resolve(parsedResult)
+        } catch (err) {
+            reject(err)
+        }
+    });
+
+    return NextResponse.json(generateAndParseContent, { status: 200 });
 }
